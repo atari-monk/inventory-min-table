a7ffccd22d1bfe33efa21018b50e2ceec7ba79b6
krzm <kmazanek@gmail.com>
2022-08-23 15:59:15 +0200

    14 columns item table

--- Inventory.Min.Table/BetterCliTableLib/ItemTable.cs
+++ Inventory.Min.Table/BetterCliTableLib/ItemTable.cs
@@ -17,21 +17,23 @@ public class ItemTable
         BuildColumn(builder, nameof(Item.Name));
         BuildColumn(builder, nameof(Item.Description));
         BuildColumn(builder, nameof(Item.Quantity));
-        BuildColumn(builder, nameof(Item.CategoryId));
+        //BuildColumn(builder, nameof(Item.CategoryId));
         BuildColumn(builder, nameof(Item.PurchaseDate));
         BuildColumn(builder, nameof(Item.PurchasePrice));
         BuildColumn(builder, nameof(Item.SellPrice));
-        BuildColumn(builder, nameof(Item.ImagePath));
-        BuildColumn(builder, nameof(Item.LengthUnitId));
+        //BuildColumn(builder, nameof(Item.ImagePath));
+        //BuildColumn(builder, nameof(Item.LengthUnitId));
         BuildColumn(builder, nameof(Item.Length));
         BuildColumn(builder, nameof(Item.Heigth));
         BuildColumn(builder, nameof(Item.Depth));
         BuildColumn(builder, nameof(Item.Diameter));
-        BuildColumn(builder, nameof(Item.VolumeUnitId));
+        //BuildColumn(builder, nameof(Item.VolumeUnitId));
         BuildColumn(builder, nameof(Item.Volume));
         BuildColumn(builder, nameof(Item.Mass));
-        BuildColumn(builder, nameof(Item.MassUnitId));
-
+        //BuildColumn(builder, nameof(Item.MassUnitId));
+        //BuildColumn(builder, nameof(Item.TagId));
+        //BuildColumn(builder, nameof(Item.StateId));
+        //BuildColumn(builder, nameof(Item.ParentId));
         Table = builder.Build();
         Table.Config = TableConfig.Unicode();
     }
@@ -45,21 +47,23 @@ public class ItemTable
                 , item.Name
                 , item.Description
                 , item.Quantity
-                , item.CategoryId
+                //, item.CategoryId
                 , item.PurchaseDate
                 , item.PurchasePrice
                 , item.SellPrice
-                , item.ImagePath
-                , item.LengthUnitId
+                //, item.ImagePath
+                //, item.LengthUnitId
                 , item.Length
                 , item.Heigth
                 , item.Depth
                 , item.Diameter
-                , item.VolumeUnitId
+                //, item.VolumeUnitId
                 , item.Volume
                 , item.Mass
-                , item.MassUnitId
-
+                //, item.MassUnitId
+                //, item.TagId
+                //, item.StateId
+                //, item.ParentId
                 );
         }
     }
@@ -74,21 +78,23 @@ public class ItemTable
                 , item.Name ?? string.Empty
                 , item.Description ?? string.Empty
                 , item.Quantity?.ToString() ?? string.Empty
-                , item.CategoryId?.ToString() ?? string.Empty
+                //, item.CategoryId?.ToString() ?? string.Empty
                 , item.PurchaseDate?.ToString(Model.DateOnlyFormat) ?? string.Empty
                 , item.PurchasePrice?.ToString(Model.MoneyFormat, CultureInfo.GetCultureInfo(Model.PolishCulture)) ?? string.Empty
                 , item.SellPrice?.ToString(Model.MoneyFormat, CultureInfo.GetCultureInfo(Model.PolishCulture)) ?? string.Empty
-                , item.ImagePath ?? string.Empty
-                , item.LengthUnitId?.ToString() ?? string.Empty
+                //, item.ImagePath ?? string.Empty
+                //, item.LengthUnitId?.ToString() ?? string.Empty
                 , item.Length?.ToString() ?? string.Empty
                 , item.Heigth?.ToString() ?? string.Empty
                 , item.Depth?.ToString() ?? string.Empty
                 , item.Diameter?.ToString() ?? string.Empty
-                , item.VolumeUnitId?.ToString() ?? string.Empty
+                //, item.VolumeUnitId?.ToString() ?? string.Empty
                 , item.Volume?.ToString() ?? string.Empty
                 , item.Mass?.ToString() ?? string.Empty
-                , item.MassUnitId?.ToString() ?? string.Empty
-
+                //, item.MassUnitId?.ToString() ?? string.Empty
+                //, item.TagId?.ToString() ?? string.Empty
+                //, item.StateId?.ToString() ?? string.Empty
+                //, item.ParentId?.ToString() ?? string.Empty
                 });
         }
         return list;
